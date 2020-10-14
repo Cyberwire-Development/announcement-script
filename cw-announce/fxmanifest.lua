162bb7123a98523e2b73bd3fefc562175cb6c173
Cyberwire Development <71550894+Cyberwire-Development@users.noreply.github.com>
2020-10-14 22:29:56 +0100

    Add files via upload

---
+++ cw-announce/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Vision'
+description 'Announcement Script'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua',
+
+}
+
+server_script 'server.lua'
